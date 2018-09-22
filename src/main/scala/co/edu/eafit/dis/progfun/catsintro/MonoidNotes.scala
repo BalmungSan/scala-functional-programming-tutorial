8e48cc12e411487ba6e5995dcd8bc3a3941bcd67
Luis Miguel Mejía Suárez <lmejias@psl.com.co>
2018-09-21 21:47:50 -0500

    Improving the Monoid's notes

--- src/main/scala/co/edu/eafit/dis/progfun/catsintro/MonoidNotes.scala
+++ src/main/scala/co/edu/eafit/dis/progfun/catsintro/MonoidNotes.scala
@@ -1,23 +1,20 @@
 package co.edu.eafit.dis.progfun.catsintro
 
-import cats.Monoid // import the Monoid type class.
-import cats.instances.int._ // brings the implicit Monoid[Int] instance to scope.
-import cats.syntax.semigroup.catsSyntaxSemigroup // provides the |+| operator for combining two monoids.
-import cats.syntax.eq.catsSyntaxEq // provides the === operator for type safe equality checks.
+import cats.Monoid // Import the Monoid type class.
+import cats.instances.int._ // Brings the implicit Monoid[Int] and Eq[Int] instances to scope.
+import cats.syntax.eq.catsSyntaxEq // Provides the === operator for type safe equality checks.
+import cats.syntax.semigroup.catsSyntaxSemigroup // Provides the |+| operator for combining two Monoids.
 
 object MonoidNotes extends App {
   // Combine two ints using the |+| operator.
   println(s"1 |+| 2 = ${1 |+| 2}")
 
-  // Explicitly obtain an implicit Monoid[Int]
-  val IntMonoid: Monoid[Int] = implicitly[Monoid[Int]]
-
-  // Probe the previous operation is equivalent to calling teh combine method explicitly.
-  println(s"(1 |+| 2) === Monoid[Int].combine(1, 2) is ${(1 |+| 2) === IntMonoid.combine(1, 2)}")
+  // Probe the previous operation is equivalent to calling the combine method explicitly.
+  println(s"(1 |+| 2) === Monoid[Int].combine(1, 2) is ${(1 |+| 2) === Monoid[Int].combine(1, 2)}")
 
   // Ensure the cat's Monoid for ints uses the integer addition as the combine operation
   // and the integer's zero as the empty element.
-  println(s"(1 |+| Monoid[Int].empty) === (1 + 0) is ${(1 |+| IntMonoid.empty) === (1 + 0)}")
+  println(s"(1 |+| Monoid[Int].empty) === (1 + 0) is ${(1 |+| Monoid[Int].empty) === (1 + 0)}")
 
   // Create a generic function for adding together all elements in a List using a Monoid.
   def addAll[T](elements: List[T])(implicit TMonoid: Monoid[T]): T =
@@ -25,7 +22,7 @@ object MonoidNotes extends App {
   println(s"addAll(List(1, 2, 3)) = ${addAll(List(1, 2, 3))}")
 
   // Let's define our own Monoid for Options.
-  implicit def TOptionMonoid[T](implicit TMonoid: Monoid[T]): Monoid[Option[T]] =
+  implicit def OptionMonoid[T](implicit TMonoid: Monoid[T]): Monoid[Option[T]] =
     new Monoid[Option[T]] {
       override val empty: Option[T] = None
       override def combine(e1: Option[T], e2: Option[T]): Option[T] = (e1, e2) match {
