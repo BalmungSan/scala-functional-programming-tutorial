9e0ec34abb618965f317759bf98e4a67187dbdb7
Luis Miguel Mejía Suárez <lmejias@psl.com.co>
2018-10-31 22:23:35 -0500

    Adding the Applicative's Notes

---
+++ src/main/scala/co/edu/eafit/dis/progfun/catsintro/ApplicativeNotes.scala
@@ -0,0 +1,49 @@
+package co.edu.eafit.dis.progfun.catsintro
+
+import cats.Applicative // Import the Monad type class.
+import cats.instances.either._ // Brings the implicit Applicative[Either[E, _]] instance to scope.
+import cats.instances.option._ // Brings the implicit Applicative[Option[_]] instance to scope.
+import cats.syntax.apply._ // Provides the tupled and mapN operators.
+import cats.syntax.option._ // Provides the option smart constructors.
+
+object ApplicativeNotes extends App {
+  // Applicative allow us to join values inside a context,
+  // in a less restrictive way than Monad's flatMap.
+  // For example, the product method takes a F[A] and a F[B]
+  // and returns a F[(A, B)].
+  // However, no sequential order is guaranteed - unlike flatMap.
+  val fa = 3.some
+  val fb = "Hello, World!".some
+  val fab = Applicative[Option[?]].product(fa, fb)
+  println(s"Given fa = Some(3) & fb = Some('Hello, World!')\t->\tApplicative[Option[?]].product(fa, fb) = ${fab}")
+
+  // We can map multiple values inside a context simultaneously,
+  // using the map2 through map22 methods.
+  val summed = Applicative[Option[?]].map3(1.some, 3.some, 5.some)(_ + _ + _)
+  println(s"Applicative[Option[?]].map3(1.some, 3.some, 5.some)(_ + _ + _) = ${summed}")
+
+  // Cats provides convenient short hands versions of the tuple and map methods.
+  val tupled = (1.some, "fun".some, true.some).tupled
+  println(s"(Some(1), Some('fun'), Some(true)).tupled = ${tupled}")
+  final case class Cat(name: String, born: Int, isMale: Boolean)
+  val mapped =
+    (
+      "BalmungSan".some,
+      1997.some,
+      None
+    ).mapN(Cat.apply)
+  println(s"(Some('BalmungSan'), Some(1997), None).mapN(Cat.apply) = ${mapped}")
+
+  // Applicative and Monads.
+  // To ensure consistent semantics, Cats’ Monad provides a standard definition
+  // of product in terms of map and flatMap.
+  // `def product[F[_]: Monad, A, B](fa: F[A], fb: F[B]): F[(A, B)] = fa.flatMap(a => fb.map(b => (a, b)))`
+  // This gives what we might think of as unexpected and less useful behavior
+  // for a number of data types - for example Either.
+  val eitherErrors =
+    Applicative[Either[List[String], ?]].product(
+      Left(List("Error 1")),
+      Left(List("Error 2"))
+    )
+  println(s"Applicative[Either[List[String], ?]].product(Left(List('Error 1')), Left(List('Error 2'))) = ${eitherErrors}")
+}
